fd33929a28edeb8873e86e0df9f2233bb72b81b0
ndiaye70 <ndiaye.pape-abdoulaye@ugb.edu.sn>
2025-01-09 16:33:18 +0100

    changement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EnteteComponent } from "./frame/entete/entete.component";
 import { MenuComponent } from "./frame/menu/menu.component";
-import { AccueuilComponent } from "./accueuil/accueuil.component";
 import { FooterComponent } from "./frame/footer/footer.component";
+//import{AccueilComponent} from "./accueil/accueil.component"
+import{AccueilComponent} from "./accueil/accueil.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EnteteComponent, MenuComponent, AccueuilComponent, FooterComponent],
+  imports: [RouterOutlet, EnteteComponent, MenuComponent,AccueilComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AngularIng3IPSL';
+  title = 'FramProject';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 
 import { Routes } from '@angular/router';
-import { AccueuilComponent } from './accueuil/accueuil.component';
+import { AccueilComponent } from './accueil/accueil.component';
 import { ListeEtudiantComponent } from './liste-etudiant/liste-etudiant.component';
 import { ListeEnseignantComponent } from './liste-enseignant/liste-enseignant.component';
 import { Error404Component } from './error404/error404.component';
 
 export const routes: Routes = [
     {path: "",redirectTo:"/acceuil",pathMatch:'full'},
-    {path: "acceuil",component:AccueuilComponent},
+    {path: "acceuil",component:AccueilComponent},
     {path:"etudiants",component:ListeEtudiantComponent},
     {path:"enseignants",component:ListeEnseignantComponent},
     {path:"",component:Error404Component}
